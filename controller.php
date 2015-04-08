1c00435289fff97248aa51c644dfeb5bcf2f4bc3
derekshorten <derekshorten@hotmail.com>
2015-04-08 15:46:44 +0100

    Changes to the  controller

--- controller.php
+++ controller.php
@@ -6,21 +6,18 @@ $country "country";
 $password = "password";
 $hostname = "localhost"; 
 
-
-
-
-$dbhandle = mysql_connect($hostname, $name, $email, $mobile, $country, $password)
-or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
+//$dbhandle = mysql_connect($hostname, $name, $email, $mobile, $country, $password)
+//or die("Unable to connect to MySQL");
+//echo "Connected to MySQL<br>";
 ?>
-
-<?php
-
 <?php
 //select a database to work with
-$selected = mysql_select_db("messaging",$dbhandle)
-  or die("Could not select messaging");
+//$selected = mysql_select_db("messaging",$dbhandle)
+  //or die("Could not select messaging");
 ?>
 
+
+<?php
+require('xampp/htdocs/
 
 ?>
