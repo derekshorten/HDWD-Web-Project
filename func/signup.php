b20c3c973d02fd369d962767ab4a84cd50742e61
derekshorten <derekshorten@hotmail.com>
2015-05-26 19:10:38 +0100

    Added register.php file  and  made changes  to signup
    Added register.php file in order to fix the  problem that I was having
    with signup but  did not need it  in the the end  after making a  change
    to the  connection to the  database within the  signup. I now can add
    new users to the  database when signing up. I will leave the  register
    .php file for  to show  what what i was trying to do here.

--- func/signup.php
+++ func/signup.php
@@ -7,18 +7,27 @@ include("database/query.php");
 $first_name = $_POST['first_name'];
  $last_name = $_POST['last_name'];
  $email=$_POST['email'];
-
  $country =$_POST['country'];
  $password =$_POST['password'];
 
 
 
-$sql = "INSERT INTO users 
-						(first_name,last_name,email, mobile_number,country, password);
-		VALUES ('$first_name','$last_name','$email', 
-		        '$country', '$password')";
+$sql = "INSERT INTO users".
+						"(first_name,
+						 last_name,
+						 email,
+						 country, 
+						 password)".
+		"VALUES ('$first_name',
+				'$last_name',
+				'$email',
+				'$country', 
+				'$password')";
 				
 			
+mysql_select_db('test_db');
+$retval = mysql_query( $sql, $conn );
+	
 	$result = $conn->query($sql);
 		 if ($result->num_rows == 0) {
 	header('Location: usersdisplay.php');
@@ -30,7 +39,7 @@ $sql = "INSERT INTO users
 	$_SESSION['userid'] = $row['id'];
 	header('Location: index.php');
 	
-
+ mysql_close($con);
 }
 
 {
